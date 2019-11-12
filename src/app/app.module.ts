e2312b0cd3a63880bea87f9c1bbfec4c4b87436f
allenaustin <allenaustin18@gmail.com>
2019-11-12 09:42:48 -0600

    Commit - HOME & Calendar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HOMEComponent } from './home/home.component';
+import { CalendarPageComponent } from './calendar-page/calendar-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HOMEComponent,
+    CalendarPageComponent
   ],
   imports: [
     BrowserModule,
